e679cd798600bd3cbc48ed1506994db24d1915f8
Aliasgar Taksali <taksalialiasgar@gmail.com>
2023-05-31 13:16:43 +0530

    Dev - Updates

--- stock_prediction.py
+++ stock_prediction.py
@@ -13,8 +13,8 @@ TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title('Stock Forecast App')
 
-# stocks = ('GOOG', 'AAPL', 'MSFT', 'GME', 'TSLA', 'TCS')
-selected_stock = st.text_input('Type stock symbol for prediction', placeholder="Ex: TCS")
+stocks = ('GOOG', 'AAPL', 'MSFT', 'GME', 'TSLA', 'TCS')
+selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
 n_years = st.slider('Years of prediction:', 1, 4)
 period = n_years * 365
