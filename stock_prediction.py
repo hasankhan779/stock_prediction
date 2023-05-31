696ddacb3f7c8228480c5c615ea2dca4eca2b918
Aliasgar Taksali <taksalialiasgar@gmail.com>
2023-05-31 13:25:46 +0530

    Dev - Updates

--- stock_prediction.py
+++ stock_prediction.py
@@ -11,14 +11,7 @@ from plotly import graph_objs as go
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
-st.title('Stock Forecast App')
-
-stocks = ('GOOG', 'AAPL', 'MSFT', 'GME', 'TSLA', 'TCS')
-selected_stock = st.selectbox('Select dataset for prediction', stocks)
-
-n_years = st.slider('Years of prediction:', 1, 4)
-period = n_years * 365
-
+data = []
 
 @st.cache_data
 def load_data(ticker):
@@ -26,9 +19,17 @@ def load_data(ticker):
     data.reset_index(inplace=True)
     return data
 
+st.title('Stock Forecast App')
+
+stocks = ('GOOG', 'AAPL', 'MSFT', 'GME', 'TSLA', 'TCS')
+selected_stock = st.text_input('Type stock symbol for prediction', placeholder="Ex: TCS")
+
+st.button(on_click=load_data(selected_stock))
+
+n_years = st.slider('Years of prediction:', 1, 4)
+period = n_years * 365
 	
 data_load_state = st.text('Loading data...')
-data = load_data(selected_stock)
 data_load_state.text('Loading data... done!')
 
 st.subheader('Raw data')
